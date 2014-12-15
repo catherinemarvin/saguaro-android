61f03242c12fe34f18f671e61c249730dc9dd5d0
Kevin Hwang <k3vinhwang@gmail.com>
2014-12-14 17:12:23 -0800

    Add api client

--- app/src/main/java/net/kevinhwang/saguaro/api/SaguaroApi.java
+++ app/src/main/java/net/kevinhwang/saguaro/api/SaguaroApi.java
@@ -1,7 +1,21 @@
 package net.kevinhwang.saguaro.api;
 
+import net.kevinhwang.saguaro.models.Food;
+
+import java.util.List;
+import retrofit.Callback;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
 /**
  * Created by khwang on 12/14/14.
  */
-public class SaguaroApi {
+
+public interface SaguaroApi {
+
+    @FormUrlEncoded
+    @GET("/meal")
+    void getMeal(@Field("money") Integer money, @Field("source") String source, Callback<MealResponse> callback);
 }
--- app/src/main/java/net/kevinhwang/saguaro/api/SaguaroApiClient.java
+++ app/src/main/java/net/kevinhwang/saguaro/api/SaguaroApiClient.java
@@ -1,7 +1,37 @@
 package net.kevinhwang.saguaro.api;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit.RestAdapter;
+import retrofit.converter.GsonConverter;
+
 /**
  * Created by khwang on 12/14/14.
  */
 public class SaguaroApiClient {
+
+    private static final Object INSTANCE_LOCK = new Object();
+    private static SaguaroApi SHARED_INSTANCE;
+    private static final String ENDPOINT = "https://hidden-river-2667.herokuapp.com/api/v1/meal";
+
+    public static SaguaroApi getClient() {
+        if (SHARED_INSTANCE == null) {
+            synchronized (INSTANCE_LOCK) {
+                if (SHARED_INSTANCE == null) {
+
+                    RestAdapter.Builder builder = new RestAdapter.Builder()
+                            .setEndpoint(ENDPOINT);
+
+                    RestAdapter adapter = builder.build();
+
+                    SHARED_INSTANCE = adapter.create(SaguaroApi.class);
+                }
+            }
+
+        }
+        return SHARED_INSTANCE;
+
+    }
 }
