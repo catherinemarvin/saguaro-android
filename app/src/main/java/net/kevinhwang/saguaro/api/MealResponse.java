527d1db3bbd76a398da26226c0e6cde5a78855c9
Kevin Hwang <k3vinhwang@gmail.com>
2014-12-14 16:34:15 -0800

    Mock out necessary Api components for consumption

---
+++ app/src/main/java/net/kevinhwang/saguaro/api/MealResponse.java
@@ -0,0 +1,7 @@
+package net.kevinhwang.saguaro.api;
+
+/**
+ * Created by khwang on 12/14/14.
+ */
+public class MealResponse {
+}
---
+++ app/src/main/java/net/kevinhwang/saguaro/models/Food.java
@@ -0,0 +1,43 @@
+package net.kevinhwang.saguaro.models;
+
+/**
+ * Created by khwang on 12/14/14.
+ */
+public class Food {
+    private Long id;
+    private String name;
+    private Integer calories;
+    private Integer price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCalories() {
+        return calories;
+    }
+
+    public void setCalories(Integer calories) {
+        this.calories = calories;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
